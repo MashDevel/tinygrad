1be76d6a5089850fb69832984631428dd86f1b29
qazal <qazal.software@gmail.com>
2025-01-16 10:34:45 +0200

    delete it

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -235,7 +235,6 @@ class UOpMetaClass(type):
 # some uops map to other stuff
 buffers:weakref.WeakKeyDictionary[UOp, Buffer] = weakref.WeakKeyDictionary() # this maps BUFFER uops to their device Buffers
 all_metadata:weakref.WeakKeyDictionary[UOp, Metadata] = weakref.WeakKeyDictionary()
-forced_realize:weakref.WeakSet[UOp] = weakref.WeakSet()
 
 # NOTE: this should be frozen, but frozen is slower
 @dataclass(eq=False, slots=True)
@@ -425,10 +424,7 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
     # NOTE: DISK uses subbuffer because DISK does not render kernels
     if self.device.startswith("DISK"): return self.alu(Ops.BUFFER_VIEW)
     # otherwise it's normal CONTIGUOUS
-    if not unwrap(self.st).contiguous or self.size != self.base.size or self.base.op is Ops.CONST:
-      return self.alu(Ops.CONTIGUOUS)
-    forced_realize.add(self.base)
-    return self
+    return self.alu(Ops.CONTIGUOUS)
 
   # *** from LazyBuffer ***
 
@@ -459,8 +455,6 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
   def lbs(self): return [self]
   @property
   def metadata(self): return all_metadata.get(self, None)
-  @property
-  def forced_realize(self): return self in forced_realize
 
   # *** uop movement ops ***
 
